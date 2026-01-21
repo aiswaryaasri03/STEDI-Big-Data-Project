851768970741e52904488f3cf3d5cc730d04b2a5
aiswaryaasri03 <101572558+aiswaryaasri03@users.noreply.github.com>
2026-01-21 13:17:54 +0530

    Update node IDs in customer landing script

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -27,18 +27,19 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node Customer Landing
-CustomerLanding_node1768971647775 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://sri-lake-house/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1768971647775")
+CustomerLanding_node1768979589657 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://sri-lake-house/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1768979589657")
 
 # Script generated for node Share with Research
 SqlQuery0 = '''
-select * from myDataSource where shareWithResearchAsOfDate is not null
+select * from myDataSource
+where shareWithResearchAsOfDate is not null;
 '''
-SharewithResearch_node1768971677473 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":CustomerLanding_node1768971647775}, transformation_ctx = "SharewithResearch_node1768971677473")
+SharewithResearch_node1768979592363 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":CustomerLanding_node1768979589657}, transformation_ctx = "SharewithResearch_node1768979592363")
 
 # Script generated for node Customer Trusted
-EvaluateDataQuality().process_rows(frame=SharewithResearch_node1768971677473, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1768971620825", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-CustomerTrusted_node1768971680882 = glueContext.getSink(path="s3://sri-lake-house/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1768971680882")
-CustomerTrusted_node1768971680882.setCatalogInfo(catalogDatabase="stedi-db",catalogTableName="customer_trusted")
-CustomerTrusted_node1768971680882.setFormat("json")
-CustomerTrusted_node1768971680882.writeFrame(SharewithResearch_node1768971677473)
+EvaluateDataQuality().process_rows(frame=SharewithResearch_node1768979592363, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1768979363798", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+CustomerTrusted_node1768979595123 = glueContext.getSink(path="s3://sri-lake-house/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1768979595123")
+CustomerTrusted_node1768979595123.setCatalogInfo(catalogDatabase="stedi-db",catalogTableName="customer_trusted")
+CustomerTrusted_node1768979595123.setFormat("json")
+CustomerTrusted_node1768979595123.writeFrame(SharewithResearch_node1768979592363)
 job.commit()
